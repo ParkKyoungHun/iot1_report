e1bdfc62cabffab9a92f2bf8fd7a0ae31fb8c4b2
kyounghunpark <redfila@naver.com>
2017-06-12 13:11:18 +0900

    test

--- iot1_report/src/report/parkkyounghun/r0002/Test.java
+++ iot1_report/src/report/parkkyounghun/r0002/Test.java
@@ -2,9 +2,17 @@ package report.parkkyounghun.r0002;
 
 public class Test {
 
+	public void printMultipleTable(int initNum, int maxNum){
+		for(int i=initNum;i<=maxNum;i++){
+			int a=1;
+			for(a=initNum;a<maxNum;a++){
+				System.out.print(i + " X " + a + " = " + (i*a) + ",");
+			}
+			System.out.println(i + " X " + a + " = " + (i*a) );
+		}
+	}
 	public static void main(String[] args){
-		int a = 1;
-		int b = 1;
-		System.out.println(a + " X " + b + " = " + (a*b));
+		Test t = new Test();
+		t.printMultipleTable(1, 5);
 	}
 }
