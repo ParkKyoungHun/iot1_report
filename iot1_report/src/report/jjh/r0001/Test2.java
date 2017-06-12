55c92c8a2c5c39681bef77f08198eb2116d52443
DJA <DJA@DJA-PC>
2017-06-12 10:57:25 +0900

    sdfg

--- iot1_report/src/report/jjh/r0001/Test2.java
+++ iot1_report/src/report/jjh/r0001/Test2.java
@@ -1,12 +1,11 @@
 package report.jjh.r0001;
 
-
+//GIT테스트용으로 커밋한건입니다.
 public class Test2 {
 	int a = 0;
 	int b = 0;
 	int c = 0;
-	// �� �ݺ����� �����ϴ� �Լ� ����Ʈ����, ����Ʈ�������������� c�� ������� ���Ͱ��� ��� �ڵ�
-
+	
 	public void printLoop() {
 		int count = 0;
 		for (int i = a; i <= b; i++) {
@@ -23,7 +22,7 @@ public class Test2 {
 		int count = 0;
 		for (int i = a; i >= b; i--) {
 			count += 1;
-			//���� �ٵ� i==b�� �ʿ��ұ��?
+			
 			if (count % c == 0 || i == b) {
 				System.out.println(i);
 			} else {
