310059614210b5816909c9280e1a29289d68663f
kyounghunpark <redfila@naver.com>
2017-06-12 13:11:43 +0900

    test

---
+++ iot1_report/src/report/parkkyounghun/r0002/ArrayExam.java
@@ -0,0 +1,12 @@
+package report.parkkyounghun.r0002;
+
+public class ArrayExam {
+
+	public static void main(String[] args){
+		int[] a = new int[4];
+		int[] b = {1,2,3,4};
+		System.out.println("a의길이 = " + a.length);
+		System.out.println("b의길이 = " + b.length);
+		System.out.println("b의 첫번째 방의 값 = " + b[0]);
+	}
+}
