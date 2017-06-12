44ad521677542a964723f1ca252fcaa81eae9745
Administrator <Administrator@MSDN-SPECIAL>
2017-06-12 11:21:59 +0900

    구구단 예제

---
+++ iot1_report/src/report/parkkyounghun/r0002/Test.java
@@ -0,0 +1,10 @@
+package report.parkkyounghun.r0002;
+
+public class Test {
+
+	public static void main(String[] args){
+		int a = 1;
+		int b = 1;
+		System.out.println(a + " X " + b + " = " + (a*b));
+	}
+}
