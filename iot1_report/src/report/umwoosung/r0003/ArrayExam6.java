cc40201037bf56e8d908c1c451a6001ddb6603cf
woosungum <redmiler@naver.com>
2017-06-13 18:50:12 +0900

    2016-06-13

--- iot1_report/src/report/umwoosung/r0003/ArrayExam6.java
+++ iot1_report/src/report/umwoosung/r0003/ArrayExam6.java
@@ -1,28 +1,26 @@
 package report.umwoosung.r0003;
 
 public class ArrayExam6 {
-	public static test1(min,max){
-		
-		int[]a = new int [10];
-		for(int min=1;i<=max;i++){
-			a[10-i] = i*2;
-		
+	public int[] getArrVar(int[]params) {
+		int[] a = new int[params[0]];
+		for (int i = params[1]; i <= params[2]; i++) {
+			a[i] = (i + 1) * 2;
+
+		}
+		return a;
 	}
-		public static test2(){
-			
-			for(int i= 1;i<=10;i++){
-				System.out.println(a[i-1]);
-			}
+
+	public void printArrVar(int[] a) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(a[i - 1]);
+
 		}
-			
-		}
+	}
+
 	public static void main(String[] args) {
-		 ArrayExam6 a6 = new ArrayExam6();
-		 int min = 1;
-		 int max = 10;
-		 
-		
-		
-		
+		ArrayExam6 ae = new ArrayExam6();
+		int[]params = (10,0,9);
+		int[] a = ae.getArrVar(params);
+		ae.printArrVar(a);
 	}
 	}
