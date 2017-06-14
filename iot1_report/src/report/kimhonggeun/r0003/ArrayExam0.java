a00a9b0d95b2ca51d9aa871992a74f27588d81e1
kimhonggeun <hong02688@gmail.com>
2017-06-14 18:09:57 +0900

    20170614

--- iot1_report/src/report/kimhonggeun/r0003/ArrayExam0.java
+++ iot1_report/src/report/kimhonggeun/r0003/ArrayExam0.java
@@ -7,16 +7,16 @@ public class ArrayExam0 {
 		for(int i=initNum;i<=maxNum;i++){
 			a[i]=(i+1)*2;
 		}
-		
+
 		return a;
 	}
-	
+
 	public void printArrVar(int[]a){
 		for(int i=1;i<=10;i++){
 			System.out.println(a[i-1]);
 		}
 	}
-	
+
 	public static void main(String[]args){
 		ArrayExam0 ae = new ArrayExam0();
 		int[] a = ae.getArrayVar(10,0,9);
