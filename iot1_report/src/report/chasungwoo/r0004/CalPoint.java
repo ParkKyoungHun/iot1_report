91a6a539005629435156051c39ea89f291b7a648
ChaSungwoo <reorio22@nate.com>
2017-06-14 12:45:54 +0900

    asdf

--- iot1_report/src/report/chasungwoo/r0004/CalPoint.java
+++ iot1_report/src/report/chasungwoo/r0004/CalPoint.java
@@ -16,22 +16,20 @@ public class CalPoint {
 		scan.close();
 		return x;
 	}
+
 	public void output(int[] input){
 
 		int k = 0;
 		for(int i=0; i<input.length; i++){
 			k += input[i];
-
 		}
 		System.out.println("총점은 : " + k + "점이며 평균 점수는 " + k/input.length + "점 입니다.");
 	}
 
 	public static void main(String[] args){
+
 		CalPoint cp = new CalPoint();
 		int[] input = cp.getVal();
 		cp.output(input);
-
-
 	}
-
 }
