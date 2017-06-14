4fdba9efeedfff9bb58f6e22c950b6d3fd13847b
llack <akak343@naver.com>
2017-06-14 18:08:36 +0900

    t

--- iot1_report/src/report/nayoungwoo/r0003/ArrayExam5.java
+++ iot1_report/src/report/nayoungwoo/r0003/ArrayExam5.java
@@ -2,40 +2,26 @@ package report.nayoungwoo.r0003;
 
 public class ArrayExam5 {
 
-//	public static int[] checkDupArray(int[] a, int checkNum) {
-//		
-//		for(int i =0; i< a.length; i++) {
-//			if(a[i] == checkNum) {
-//				a[i] = (int) (Math.random() * 45);
-//				checkDupArray(a,(int) (Math.random() * 45));
-//			}
-//		}
-//		return a;
-//	}
-
 	public static void main(String args[]) {
 
 		int[] a = new int[6];
 		
-		for (int i = 0; i <6; i++) {
+		for (int i = 0; i <a.length; i++) {
 
 			int random = (int) (Math.random() * 45) + 1;
 			a[i] = random;
-	
-			System.out.println((i + 1) + "번째 로또번호 = " + a[i]);
 			
-			for (int j= i+1; j >=0;j--) {
-				if(j<7 && a[j] == a[i] && i!=j){
+			for (int j= i; j >=0;j--) {
+				if(a[j] ==  a[i] && i!=j){
 					j=0;
 					i--;
 				}
 			}
 		}
-
+		for (int i =0; i< a.length; i++) {
+			System.out.println((i + 1) + "번째 로또번호 = " + a[i]);
+			
+		}
 	}
 
 }
-
-// d
-// aj 와 ai 가 같다며언언언언언언어ㅓ어어어어어                    
-//      noi no no no no no no no no no no no no non o
