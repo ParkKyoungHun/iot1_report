4b6084c3cf75e44d31cdde1b33a758fd5495d20d
kyounghunpark <redfila@naver.com>
2017-06-14 17:18:51 +0900

    tt

--- iot1_report/src/report/parkkyounghun/r0003/ArrayExam7.java
+++ iot1_report/src/report/parkkyounghun/r0003/ArrayExam7.java
@@ -10,19 +10,31 @@ public class ArrayExam7 {
 		for(int i=0;i<a.length;i++){
 			if(a[i]==random){
 				random = (int)(Math.random()*45)+1;
-				i--;
+				System.out.println(i + ":" + random);
+				i=-1;
 			}
 		}
 		return random;
 	}
 	public static void main(String[] args){
-		int[] a = new int[10];
+		int[] a = new int[45];
 		ArrayExam7 ae5 = new ArrayExam7();
-		for(int i=0;i<10;i++){
+		for(int i=0;i<45;i++){
 			a[i] = ae5.checkNum(a);
 		}
-		for(int i=0;i<10;i++){
-			System.out.println((i+1) + "번째 로또번호는 : " + a[i]);
+		
+		for(int i=0;i<45;i++){
+			for(int j=i+1;j<45;j++){
+				if(a[i]<a[j]){
+					int tmp = a[i];
+					a[i] = a[j];
+					a[j] = tmp;
+				}
+			}
+		}
+
+		for(int i=0;i<45;i++){
+			System.out.println(i + "번째 로또번호는 : " + a[i]);
 		}
 	}
 }
