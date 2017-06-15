afe7eb76ac750a3c5101e7811967eb0ea44765e6
SongWooJae <woojae9532@naver.com>
2017-06-15 15:10:24 +0900

    2017-06-15

--- iot1_report/src/report/parkkyounghun/r0004/CalPoint2.java
+++ iot1_report/src/report/parkkyounghun/r0004/CalPoint2.java
@@ -6,12 +6,12 @@ public class CalPoint2 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int result = 0;
-		int[] num = new int[2];
+		int[] num = new int[5];
 		for(int i=0;i<num.length;i++){
 			System.out.println((i+1) + "번째 학생의 점수를 입력해주세요=>:");
 			String numStr = scan.nextLine();
 			num[i] = Integer.parseInt(numStr);
-			result += num[i];
+			result += num[i]; //result = result + num[i];
 		}
 		System.out.println("입력하신 5학생의 점수의 총점 = " + (result));
 		System.out.println("입력하신 5학생의 평균 점수 = " + (result/num.length));
