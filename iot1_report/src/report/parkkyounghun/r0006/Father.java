7c075b885f815f88ae0d88ae038a486e7986e397
imhyejin <imagine.codes@gmail.com>
2017-06-16 14:37:30 +0900

    2017-06-16

--- iot1_report/src/report/parkkyounghun/r0006/Father.java
+++ iot1_report/src/report/parkkyounghun/r0006/Father.java
@@ -4,19 +4,20 @@ public class Father {
 	private String address = "서울시 강서구 가양동";
 	private int age = 65;
 	private String name = "박철수";
-	
-	int getAge(){
+
+	int getAge() {
 		return this.age;
 	}
-	String getAdress(){
+
+	String getAdress() {
 		return address;
 	}
-	
-	String getName(){
+
+	String getName() {
 		return name;
 	}
-	
-	void setName(String name){
+
+	void setName(String name) {
 		this.name = name;
 	}
 }
