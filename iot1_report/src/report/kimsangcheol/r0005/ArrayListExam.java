17135d3e856d0bf1bdeeec5921427f2a10a24d06
sangcheolkim <tkdral@naver.com>
2017-06-16 12:35:25 +0900

    2017-06-16

--- iot1_report/src/report/kimsangcheol/r0005/ArrayListExam.java
+++ iot1_report/src/report/kimsangcheol/r0005/ArrayListExam.java
@@ -10,13 +10,14 @@ public class ArrayListExam {
 		ArrayList<ArrayExam3> aLE3 = new ArrayList<ArrayExam3>();
 		ArrayExam3 aE3= new ArrayExam3();
 		
-		aE3.getArrayVar(0, 6);
+		
+		
 		aLE3.add(aE3);
 		
-		for(int i=0; i>aLE3.size();i++){
-			aLE3.get(i);
+		for(int i=0; i<aLE3.size();i++){
+			System.out.println(aLE3.get(i));		//주소지 출력
 			
 		}
 	}
 
-}								//멘탈 조정 필요;;;;
+}								
--- iot1_report/src/report/kimsangcheol/r0006/Son.java
+++ iot1_report/src/report/kimsangcheol/r0006/Son.java
@@ -3,19 +3,19 @@ package report.kimsangcheol.r0006;
 public class Son extends Father{			//Father 를 상속 받음. <다중 상속 불가능>
 	int a;
 	
-	int getAge(){							//상속 받은 함수들은 재정의를 할 수 있음. Overriding
-		age =25;
-		return age;
-	}
+											//상속 받은 함수들은 재정의를 할 수 있음. Overriding[함수의 재정의]
+
 	public static void main(String[] args){
 		Son s = new Son();
 		s.a = 3;
-		System.out.println(s.age);
+		System.out.println(s.getAddress());
 		System.out.println(s.getAge());
-		System.out.println(s.age);
+		System.out.println(s.getName());
+		s.setAge(20);
+		System.out.println(s.getAge());
 	
-//		Father f = new Father();
-//		System.out.println(f.address);
+		Father f = new Father();
+		System.out.println(f.getAddress());
 		
 	}
 }
