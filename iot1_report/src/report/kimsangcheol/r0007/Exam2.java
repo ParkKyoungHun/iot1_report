2fcd7cf77975db33d7aa7b853cf3e473dc31de16
sangcheolkim <tkdral@naver.com>
2017-06-19 11:39:15 +0900

    2017-06-19

--- iot1_report/src/report/kimsangcheol/r0007/Exam2.java
+++ iot1_report/src/report/kimsangcheol/r0007/Exam2.java
@@ -1,5 +1,7 @@
 package report.kimsangcheol.r0007;
 
+import report.kimsangcheol.r0006.RExam4;
+
 public class Exam2 {
 	public static void main(String[]args){
 		Exam1 e1 = new Exam1();
@@ -11,5 +13,8 @@ public class Exam2 {
 		e1 = new Exam1();
 		System.out.println(e2.a);
 		System.out.println(e2.b);
+		RExam4 e4 = new RExam4();
+		System.out.println(e4.a);
+		System.out.println(e4.b);
 	}
 }
