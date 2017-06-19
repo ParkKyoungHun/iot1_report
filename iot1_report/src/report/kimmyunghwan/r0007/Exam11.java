5674327801147a28edebc50ec6e4c274eb08f5a5
kimmynughwan <ralphmh@gmail.com>
2017-06-19 17:20:46 +0900

    2017-06-19

---
+++ iot1_report/src/report/kimmyunghwan/r0007/Exam11.java
@@ -0,0 +1,9 @@
+package report.kimmyunghwan.r0007;
+
+public class Exam11 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ iot1_report/src/report/kimmyunghwan/r0007/Exam6.java
@@ -0,0 +1,13 @@
+package report.kimmyunghwan.r0007;
+
+import report.kimmyunghwan.r0006.RExam2_1;
+
+public class Exam6 extends RExam2_1 {
+
+	public static void main(String[] args){
+		//RExam2_1 re2 = new RExam2_1();  //이 클래스가 r0007에 있어서 c+s+o 임포트해줌
+		Exam6 e6 = new Exam6();
+		e6.a = 3;
+		System.out.println(e6.a);
+	}
+}
