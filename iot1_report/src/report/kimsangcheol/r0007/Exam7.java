6a21332b0d16987fa715613575662037c98b9d14
sangcheolkim <tkdral@naver.com>
2017-06-19 14:35:13 +0900

    2017-06-19

---
+++ iot1_report/src/report/kimsangcheol/r0007/Exam7.java
@@ -0,0 +1,28 @@
+package report.kimsangcheol.r0007;
+
+public class Exam7 {
+	private int a;
+	private int b;
+	private int c;
+	Exam7(int a, int b, int c){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	int getA(int num){
+		this.a = num;
+		return a;
+	}
+	int getB(int num){
+		this.b = num;
+		return b;
+	}
+	int getC(int num){
+		this.c = num;
+		return c;
+	}
+	public static void main(String[]args){
+		
+	}
+	
+}
