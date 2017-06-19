2dd9665519fc3fa948dea5987fc8f0b41e1fff7b
sangcheolkim <tkdral@naver.com>
2017-06-19 14:45:34 +0900

    2017-06-19

--- iot1_report/src/report/kimsangcheol/r0007/Exam8.java
+++ iot1_report/src/report/kimsangcheol/r0007/Exam8.java
@@ -3,7 +3,7 @@ package report.kimsangcheol.r0007;
 public class Exam8 {
 	
 	public static void main(String[]args){
-		Exam7 e7 = new Exam7(0,0,0);
+		Exam7 e7 = new Exam7(5,6,9);
 		System.out.println(e7.getA());
 		System.out.println(e7.getB());
 		System.out.println(e7.getC());
