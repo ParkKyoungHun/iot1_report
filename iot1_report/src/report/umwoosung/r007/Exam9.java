ba309615338315dc478072cbeabf09b816949c9f
woosungum <redmiler@naver.com>
2017-06-19 18:37:08 +0900

    2017.06.19

--- iot1_report/src/report/umwoosung/r007/Exam9.java
+++ iot1_report/src/report/umwoosung/r007/Exam9.java
@@ -1,31 +1,33 @@
 package report.umwoosung.r007;
 
+import java.util.Scanner;
+
 public class Exam9 {
+	private int a;
+	private int b;
+	private int result;
+	Scanner scan;
 	
-		private int a;
-		private int b;
-		private int result;
-
-		Scanner scan;
-		public Exam9(){
-
-			scan = new Scanner(System.in);
-			
-			this.a = a;
-			this.b = b;
-			this.result = result;
-		
-			
-		}
-		Scanner scan = new Scanner(System.in);
-		int a = Integer.parseInt(nextLine());
-		int b = Integer.parseInt(nextLine());
-		for(int i = a;i<b;i++){
-			
-		}
-		
-		
-		
+	public Exam9(){ //생성자는 초기 값을 생성할 때 많이 사용한다.
+		scan = new Scanner(System.in);
 	}
+	public void inputA(){
+		System.out.print("초기 값을 입력해주세요. => ");
+		this.a = Integer.parseInt(scan.nextLine());
+	}
+	public void inputB(){
+		System.out.print("마지막 값을 입력해주세요. => ");
+		this.b = Integer.parseInt(scan.nextLine());
+	}
+	
+	public void result(){  //계산하는 함수.
+		for(int i = a; i<=b; i++){
+			result += i;
+		}	
+	}
+	public int printResult(){ // 출력하는 함수.
+		return result;
+	}
+// 계산과 출력하는 함수는 각각 만들어서 사용하기!!.
 
 }
