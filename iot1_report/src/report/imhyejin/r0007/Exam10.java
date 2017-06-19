15a497565da37c063d4ae82066d913b6b773ec5b
imhyejin <imagine.codes@gmail.com>
2017-06-19 18:35:06 +0900

    2017-06-19

--- iot1_report/src/report/imhyejin/r0007/Exam10.java
+++ iot1_report/src/report/imhyejin/r0007/Exam10.java
@@ -4,10 +4,6 @@ public class Exam10 {
 
 	protected int a, b, result;
 
-	// a와 b값을 초기화 하는 Exam10생성자를 만들어주세요.
-	// a와 b값을 더하기, 빼기, 나누기, 곱하기 하는 함수를 각각 4개 작성해주시고
-	// 해당연산결과는 printResult() 라는 함수를 호출했을 때 나오게 프로그램을 작성해주시면 됩니다.
-	// 호출은 Exam11라는 클래스를 만들어서 그안에서 해주시기 바랍니다.
 
 	public Exam10() {
 	}
