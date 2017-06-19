163ca626d146c34ed95c2d5a4ad920ac7dac4444
sangcheolkim <tkdral@naver.com>
2017-06-19 16:17:18 +0900

    2017-06-19

--- iot1_report/src/report/kimsangcheol/r0007/Exam9.java
+++ iot1_report/src/report/kimsangcheol/r0007/Exam9.java
@@ -8,9 +8,11 @@ public class Exam9 {
 	private int result;
 	Scanner scan = new Scanner(System.in);
 	
-	void getAB(){
+	void getA(){
 		System.out.println("초기값을 입력해주세요.>");
 		a=Integer.parseInt(scan.nextLine());
+	}
+	void getB(){
 		System.out.println("종료값을 입력해주세요.>");
 		b=Integer.parseInt(scan.nextLine());
 		
