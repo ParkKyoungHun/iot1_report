9320d490458034b76d81f2cbe2cfead56ea4cf96
ChaSungwoo <reorio22@nate.com>
2017-06-19 18:16:49 +0900

    please sleep early

---
+++ iot1_report/src/report/chasungwoo/r0007/Exam10.java
@@ -0,0 +1,15 @@
+package report.chasungwoo.r0007;
+
+public class Exam10 {
+	/* Exam9에 private int a,b,result를 선언해주세요
+	 * i가 a부터 b까지 도는 반복문을 작성한 뒤
+	 * result의 i를 반복문만큼 더하는 함수를 작성해주세요
+	 * result의 값을 출력하는 함수를 작성해서 Exam10에서 출력해주시기 바랍니다.
+	 */
+	public static void main(String[] args){
+		Exam9 ex9 = new Exam9();
+		ex9.scanner();
+		ex9.loop();
+		ex9.printResult();
+	}
+}
