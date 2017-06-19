3916f096b2b63aa642a69b8026f0d14f8dcb8848
kimmynughwan <ralphmh@gmail.com>
2017-06-19 18:36:37 +0900

    20170619

--- iot1_report/src/report/kimmyunghwan/r0008/Exam12.java
+++ iot1_report/src/report/kimmyunghwan/r0008/Exam12.java
@@ -2,14 +2,15 @@ package report.kimmyunghwan.r0008;
 
 import report.kimmyunghwan.r0007.Exam11;
 
-public class Exam12 extends Exam11{
+public class Exam12 extends Exam11 {
 
-	Exam12(int a, int b){
+	Exam12(int a, int b) {
 		this.a = a;
 		this.b = b;
 	}
-	public static void main(String[] args){
-		Exam12 e12 =new Exam12(1,2);
+
+	public static void main(String[] args) {
+		Exam12 e12 = new Exam12(1, 2);
 		e12.plus();
 		e12.printResult();
 	}
