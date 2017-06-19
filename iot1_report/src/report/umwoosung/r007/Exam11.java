89e50fe7977c01365a839915487066c292336050
woosungum <redmiler@naver.com>
2017-06-19 18:24:24 +0900

    asdf

---
+++ iot1_report/src/report/umwoosung/r007/Exam11.java
@@ -0,0 +1,17 @@
+package report.umwoosung.r007;
+//a와 b값을 초기화하는 Exam11생성자를 만들어주세요
+//a와 b값을 더하기,빼기,나누기,곱하기 하는 함수를 각각4개 작성해주세요
+///해당연산결과는 printResult()라는 함수를 호출했을때 나오게 프로그램을 작성해주시면됩니다.abstract
+
+public class Exam11 {
+	
+	int a;
+	int b;
+
+	
+	public void printResult(){
+		System.out.println(result);
+		
+	}
+
+}
---
+++ iot1_report/src/report/umwoosung/r007/Exam9.java
@@ -0,0 +1,31 @@
+package report.umwoosung.r007;
+
+public class Exam9 {
+	
+		private int a;
+		private int b;
+		private int result;
+
+		Scanner scan;
+		public Exam9(){
+
+			scan = new Scanner(System.in);
+			
+			this.a = a;
+			this.b = b;
+			this.result = result;
+		
+			
+		}
+		Scanner scan = new Scanner(System.in);
+		int a = Integer.parseInt(nextLine());
+		int b = Integer.parseInt(nextLine());
+		for(int i = a;i<b;i++){
+			
+		}
+		
+		
+		
+	}
+
+}
