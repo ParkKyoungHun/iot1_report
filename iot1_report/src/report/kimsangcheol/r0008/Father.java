32e19a44b9c8fe78d7f50aadfecf66fb8f7a07ff
sangcheolkim <tkdral@naver.com>
2017-06-20 17:34:34 +0900

    2017-06-20

--- iot1_report/src/report/kimsangcheol/r0008/Father.java
+++ iot1_report/src/report/kimsangcheol/r0008/Father.java
@@ -3,13 +3,24 @@ package report.kimsangcheol.r0008;
 import java.util.Scanner;
 
 public class Father {
-	int a=3;
+	int[] score= new int[10];
+	int[] stud = new int[10];
 	Scanner scan;
 	String str1, str2;
+	int initNum, maxNum, temp;
 	Father(){
 		scan = new Scanner(System.in);
 		System.out.println("기본 아빠 생성자 호출!!!");
+		inputFromScanner1();
+		inputFromScanner2();
+		castToInt();
+		scoreArr();
 	}
+	void castToInt(){
+		initNum = Integer.parseInt(str1);
+		maxNum = Integer.parseInt(str2);
+	}
+
 	void inputFromScanner1(){
 		str1=scan.nextLine();
 	}
@@ -17,19 +28,38 @@ public class Father {
 		str2=scan.nextLine();
 	}
 	void printInput(){
-		inputFromScanner1();
-		inputFromScanner2();
-		int num1 = Integer.parseInt(str1);
-		int num2 = Integer.parseInt(str2);
-		if (num1>num2){
-			int temp;
-			temp=num1;
-			num1=num2;
-			num2=temp;
+		if (initNum>maxNum){
+			temp=initNum;
+			initNum=maxNum;
+			maxNum=temp;
 		}else{
 		}
-		for(int i=num1;i<=num2;i++){
+		for(int i=initNum;i<=maxNum;i++){
 			System.out.println(i);
+		}
+	}
+	void scoreArr(){
+		for(int i=0;i<score.length;i++){
+			System.out.println((i) + "번째 학생의 점수를 입력해 주세요.>");
+			score[i]=Integer.parseInt(scan.nextLine());
+			stud[i]=i;
+		}
+		for(int i=0;i<score.length;i++){
+			for(int j=i+1;j<score.length;j++){
+				if(score[i]<score[j]){
+					temp=score[i];
+					score[i]=score[j];
+					score[j]=temp;
+					temp=stud[j];
+					stud[j]=stud[i];
+					stud[i]=temp;
+				}
+			}
+		}
+	}
+	void printScore(){
+		for(int i=0;i<score.length;i++){
+			System.out.println(stud[i] + "번째 학생의 점수는 : " + score[i]);
 		}
 	}
 }
--- iot1_report/src/report/kimsangcheol/r0008/Son.java
+++ iot1_report/src/report/kimsangcheol/r0008/Son.java
@@ -12,7 +12,7 @@ public class Son extends Father{
 	public static void main(String[]args){
 		Son s = new Son();
 		s.printInput();
-		
+		s.printScore();
 	}
 
 }
