0384240f9af131eabd631f3a4d63f196c1cf1921
sangcheolkim <tkdral@naver.com>
2017-06-20 16:29:08 +0900

    2017-06-20

--- iot1_report/src/report/kimsangcheol/r0008/Father.java
+++ iot1_report/src/report/kimsangcheol/r0008/Father.java
@@ -1,12 +1,35 @@
 package report.kimsangcheol.r0008;
 
+import java.util.Scanner;
+
 public class Father {
-	
+	int a=3;
+	Scanner scan;
+	String str1, str2;
 	Father(){
-		this(1);
+		scan = new Scanner(System.in);
 		System.out.println("기본 아빠 생성자 호출!!!");
 	}
-	Father(int a){
-		System.out.println("오버로딩으로 추가한 아빠 생성자 호출!!!");
+	void inputFromScanner1(){
+		str1=scan.nextLine();
+	}
+	void inputFromScanner2(){
+		str2=scan.nextLine();
+	}
+	void printInput(){
+		inputFromScanner1();
+		inputFromScanner2();
+		int num1 = Integer.parseInt(str1);
+		int num2 = Integer.parseInt(str2);
+		if (num1>num2){
+			int temp;
+			temp=num1;
+			num1=num2;
+			num2=temp;
+		}else{
+		}
+		for(int i=num1;i<=num2;i++){
+			System.out.println(i);
+		}
 	}
 }
