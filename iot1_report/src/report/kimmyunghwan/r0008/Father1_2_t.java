d21ca6ee46a4abe66b0e64daf13bdac847d81cee
kimmynughwan <ralphmh@gmail.com>
2017-06-20 17:40:17 +0900

    20170620

--- iot1_report/src/report/kimmyunghwan/r0008/Father1_2_t.java
+++ iot1_report/src/report/kimmyunghwan/r0008/Father1_2_t.java
@@ -2,23 +2,29 @@ package report.kimmyunghwan.r0008;
 
 import java.util.Scanner;
 
-public class Father1_1 {
+public class Father1_2_t {
 	int a = 3;
 	Scanner scan;
 	String str1, str2;
 	int num1, num2;
-	//int result = 0;
 
-	Father1_1() {
+	Father1_2_t() {
 		scan = new Scanner(System.in);
 		System.out.println("아빠 기본생성자 호출!!");
+		inputFormScanner1();
+		inputFormScanner2();
+		castStringToInt();
+	}
+
+	void castStringToInt() {
+		num1 = Integer.parseInt(str1);
+		num2 = Integer.parseInt(str2);
 	}
 
 	void inputFormScanner1() {
 		System.out.println("초기 값을 입력해 주세요 ==> ");
 
 		str1 = scan.nextLine();
-		num1 = Integer.parseInt(str1);
 
 	}
 
@@ -26,18 +32,15 @@ public class Father1_1 {
 		System.out.println("맥스 값을 입력해 주세요 ==> ");
 
 		str2 = scan.nextLine();
-		num2 = Integer.parseInt(str2);
+
 	}
 
-	void printResult() {
+	void printIWithLoop() {
 
 		for (int i = num1; i <= num2; i++) {
-			//result += i;
-			System.out.print(i+",");
+
+			System.out.println(i);
 		}
-			//System.out.print("초기값 + 맥스값 : " + result + " 입니다");
-			/* "초기값 + 맥스값 : " + result + " 입니다" */
-		
 
 	}
 }
