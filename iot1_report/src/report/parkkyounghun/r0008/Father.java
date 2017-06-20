5eabb65b560794f0a983685508dd2391aafd58a9
kyounghunpark <redfila@naver.com>
2017-06-20 14:36:31 +0900

    a

--- iot1_report/src/report/parkkyounghun/r0008/Father.java
+++ iot1_report/src/report/parkkyounghun/r0008/Father.java
@@ -6,6 +6,6 @@ public class Father {
 		System.out.println("아빠 기본생성자 호출!!");
 	}
 	Father(int a){
-		System.out.println("오버로딩 개념으로 추가된 생성자 호출!!");
+		System.out.println("오버로딩 개념으로 추가된 아빠 생성자 호출!!");
 	}
 }
