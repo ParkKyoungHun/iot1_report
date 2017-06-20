2e28e56f2abbd9a09ef6fb2fa753edad12c0baf1
jaehyunjang <4jh8@naver.com>
2017-06-20 18:41:22 +0900

    2017-06-20 work

---
+++ iot1_report/src/report/jangjaehyun/r0008/Son1.java
@@ -0,0 +1,15 @@
+package report.jangjaehyun.r0008;
+
+public class Son1 extends Father1 {
+	Son1() {
+
+	}
+
+	Son1(int a) {
+	}
+
+	public static void main(String[] args) {
+		Son1 s1 = new Son1();
+		s1.printPoint();
+	}
+}
