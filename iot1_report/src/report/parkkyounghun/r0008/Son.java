4906462322af3a35ac80a8544ccabdc796adb941
kyounghunpark <redfila@naver.com>
2017-06-20 15:28:48 +0900

    adsf

--- iot1_report/src/report/parkkyounghun/r0008/Son.java
+++ iot1_report/src/report/parkkyounghun/r0008/Son.java
@@ -4,9 +4,6 @@ public class Son extends Father{
 
 	Son(){
 		System.out.println("아들 기본생성자 호출!!");
-		System.out.println("아들 기본생성자 호출!!");
-		System.out.println("아들 기본생성자 호출!!");
-		System.out.println("아들 기본생성자 호출!!");
 	}
 	Son(int a){
 		System.out.println("오버로딩 개념으로 추가된 아들 생성자 호출!!");
