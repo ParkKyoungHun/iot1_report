7fc3af9a2aa4d93efc3ff0246a993c48ef2c0432
ChaSungwoo <reorio22@nate.com>
2017-06-20 15:33:30 +0900

    who watches the watchman?

--- iot1_report/src/report/chasungwoo/r0008/Father.java
+++ iot1_report/src/report/chasungwoo/r0008/Father.java
@@ -1,12 +1,12 @@
 package report.chasungwoo.r0008;
 
 public class Father {
-	Father(){
-		System.out.println("calling my son now");
-	}
-	public static void main(String[] args){
-		Son s = new Son();
-		System.out.println(s);
-	}
+	int a = 3;
 
+	Father(){
+		System.out.println("아빠 기본생성자 호출!!");
+	}
+	Father(int a){
+		System.out.println("오버로딩 개념으로 추가된 아빠 생성자 호출!!");
+	}
 }
--- iot1_report/src/report/chasungwoo/r0008/Son.java
+++ iot1_report/src/report/chasungwoo/r0008/Son.java
@@ -1,9 +1,17 @@
 package report.chasungwoo.r0008;
 
+import report.chasungwoo.r0008.Father;
+
 public class Son extends Father{
-	
+
+	Son(){
+		System.out.println("아들 기본생성자 호출!!");
+	}
+	Son(int a){
+		System.out.println("오버로딩 개념으로 추가된 아들 생성자 호출!!");
+	}
 	public static void main(String[] args){
 		Son s = new Son();
 	}
-
 }
+
