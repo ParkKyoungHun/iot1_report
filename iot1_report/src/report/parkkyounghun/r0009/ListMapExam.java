987ac6001ed606f9c89360684cc1521d7c29adb4
kyounghunpark <redfila@naver.com>
2017-06-21 14:42:33 +0900

    t

---
+++ iot1_report/src/report/parkkyounghun/r0009/ListMapExam.java
@@ -0,0 +1,10 @@
+package report.parkkyounghun.r0009;
+
+public class ListMapExam {
+	// 스트링,인티져로 구성된 HashMap 변수를 선언해주세요
+	// HashMap변수명은 pointHm이라고 해주세요
+	// pointHm에 학생의 이름키값과 점수 밸류값을 10개 넣어주세요
+	// 학생이름은 A군, B군, C군..... 으로 입력해주세요.
+	// 해당 pointHm을 ArrayList에 추가해주세요.
+	// 반복문을 사용하여 ArrayList에 추가된 pointHm을 출력해주세요.
+}
