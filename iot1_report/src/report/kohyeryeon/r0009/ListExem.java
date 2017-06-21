dda6c3ec8e99bdd0f0eefad5234e71e825856199
kohyeryeon <rhofus17@naver.com>
2017-06-21 18:25:04 +0900

    20170621

--- iot1_report/src/report/kohyeryeon/r0009/ListExem.java
+++ iot1_report/src/report/kohyeryeon/r0009/ListExem.java
@@ -9,11 +9,11 @@ public class ListExem {
 		
 		ArrayList<String> arrList = new ArrayList<String>();
 		for(int i=0;i<=3;i++){
-			arrList.add("" + i); 
+			arrList.add("" + i);   //합하다?더하다.
 		}
 		
-		Collections.sort(arrList);
-		
+		//Collections.sort(arrList);   //오름차순~
+		Collections.reverse(arrList);   //내림차순~
 		for(int i=0;i<arrList.size();i++){     //size()는 arrList안에잇는 함수.
 			System.out.println(arrList.get(i));
 		}
