23180f7860925b07b614dc056468bb61de75ce5a
kimhonggeun <hong02688@gmail.com>
2017-06-21 17:52:38 +0900

    20170621

---
+++ iot1_report/src/report/kimhonggeun/r0009/MapExam2.java
@@ -0,0 +1,17 @@
+package report.kimhonggeun.r0009;
+
+import java.util.HashMap;
+
+public class MapExam2 {
+	
+	int a;
+	HashMap<String, Integer> hm;
+	
+	MapExam2(){
+		hm = new HashMap<String, Integer>();
+	}
+	public static void main(String[]args){
+		MapExam2 me = new MapExam2();
+		me.hm.put("1",1);
+	}
+}
