9dae61371dd0828612fe0b9fa26538ec88578705
woosungum <redmiler@naver.com>
2017-06-22 13:33:37 +0900

    dfdsf

---
+++ iot1_report/src/report/umwoosung/r0010/Today10.java
@@ -0,0 +1,11 @@
+package report.umwoosung.r0010;
+
+/*Next 다음레코드로 이동한다.
+ * while() 파라메터값이 만족할때까지 한다.
+ * add 더하는기능
+ * 
+ * containsKey("key")key 값을 가지고있는지 없는지 확인하는 명령어.
+ * 
+ * toString 그냥 Object에 선언되어있는 값일뿐이고 toString으로 오버라이딩한것뿐 그이상 그이하의 의미는 없다.
+ * 
+ */
