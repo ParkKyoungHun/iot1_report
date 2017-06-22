d025e30936ac7435775bf6c1a6c969f638552de3
jaehyunjang <4jh8@naver.com>
2017-06-22 18:48:45 +0900

    2017-06-22

---
+++ iot1_report/src/report/jangjaehyun/r0010/Exam4.java
@@ -0,0 +1,11 @@
+package report.jangjaehyun.r0010;
+
+import java.util.Arrays;
+
+public class Exam4 {
+// 1. 생성자와 함수의 차이를 서술해주세요.
+// 2. 오버로딩에 대해 서술해주세요.
+// 3. MY-SQL이란 무엇인지 서술해주세요.
+// 4. MY-SQL의 설치 방법을 서술해주세요.
+	
+}
