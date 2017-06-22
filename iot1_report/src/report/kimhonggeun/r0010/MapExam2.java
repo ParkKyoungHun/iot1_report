ebcf91daa5dc359908d2550172dcddbfa9468390
kimhonggeun <hong02688@gmail.com>
2017-06-22 16:54:15 +0900

    20170622

--- iot1_report/src/report/kimhonggeun/r0010/MapExam2.java
+++ iot1_report/src/report/kimhonggeun/r0010/MapExam2.java
@@ -44,12 +44,16 @@ public class MapExam2 extends HashMap{
 }
 
 
-
+//네트워크에 대해 서술하시오
+//내컴퓨터속성 눌러서 os 랑 비트값확인, jdk 웹에서 다운, 이클립스 웹에서 다운, 톰캣 다운
+//이클립스에 톰캣설정 - 아파치선택-톰캣8.5-피니쉬!
+//http의 기본포트 =80
+//톰캣에서 포트를 바꾸는 방법 서술 = 서버 톰캣더블클릭 포트에있는 숫자 변경
 //오버로딩서술
 //생성자 함수의 차이
 
 //총점 평균 차순
-
+//국제네트워크 표준모델인 osi7 계층도를 작성해주세요
 //mysql이란 개인으로 쓰면 무료
 //관계형 데이터베이스 시스템
 //mysql설치방법 서술
@@ -58,3 +62,17 @@ public class MapExam2 extends HashMap{
 //fsutil file createnew test.txt 1 
 //mkdir test
 //cd \w
+
+//iot_test라는 데이터베이스를 생성하는 sql을 작성해주셈
+//iot_test에서 아래의구조를 가진 테이블을 생성해주세요.
+//create database iot_test; ->컨트롤 시프트 f9-> 언네임 누르고 새로고침
+//use iot_test;
+//create table TEST(
+//id varchar(30),
+//pwd varchar(30),
+//name varchar(30)
+//);
+//-테이블명 TEST
+//-ID VARCHAR(30)
+//-PWD VARCHAR(30)
+//-NAME VARCHAR(30)
