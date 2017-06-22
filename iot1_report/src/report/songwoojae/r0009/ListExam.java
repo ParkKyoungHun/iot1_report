4ceebd9e997bf9f432087742a79d6155637c6ca6
sangcheolkim <tkdral@naver.com>
2017-06-22 17:40:16 +0900

    21

--- iot1_report/src/report/songwoojae/r0009/ListExam.java
+++ iot1_report/src/report/songwoojae/r0009/ListExam.java
@@ -5,17 +5,15 @@ import java.util.Collections;
 
 public class ListExam {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		ArrayList<Integer> arrList = new ArrayList<Integer>();
-		for (int i = 4; i >= 0; i--) {
+		for(int i=4;i>=0;i--){
 			arrList.add(((int)(Math.random()*45)+1));
 		}
-		
 		Collections.sort(arrList);
-		
-		for (int i = 0; i < arrList.size(); i++) {
+
+		for(int i=0;i<arrList.size();i++){
 			System.out.println(arrList.get(i));
 		}
 	}
-
 }
--- iot1_report/src/report/songwoojae/r0009/OverExam.java
+++ iot1_report/src/report/songwoojae/r0009/OverExam.java
@@ -2,14 +2,13 @@ package report.songwoojae.r0009;
 
 public class OverExam {
 	public String toString(int a){
-		return "ㅇ머?";
+		return "어머?";
 	}
 	public String toString(){
-		return "ㅇ머?";
+		return "어머?";
 	}
 	public static void main(String[] args){
 		OverExam oe = new OverExam();
 		System.out.println(oe);
 	}
-
 }
