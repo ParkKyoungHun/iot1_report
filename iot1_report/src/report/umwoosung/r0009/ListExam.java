a81dd41242802a25739708f20cc7068e2508eea0
DJA <DJA@DJA-PC>
2017-06-22 18:43:49 +0900

    2017.06.22

--- iot1_report/src/report/umwoosung/r0009/ListExam.java
+++ iot1_report/src/report/umwoosung/r0009/ListExam.java
@@ -2,13 +2,14 @@ package report.umwoosung.r0009;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
-public class ListExam {
+public class ListExam extends HashMap {
 
 	public static void main(String[] args){
-		ArrayList<Integer> arrList = new ArrayList<Integer>();
-		for(int i=4;i>=0;i--){
-			arrList.add(((int)(Math.random()*45)+1));
+		ArrayList<HashMap<String,Integer>> arrList = new ArrayList<HashMap<String,Integer>>();
+		for(int i=0;i<=10;i++){
+			arrList("A",2);
 		}
 		Collections.sort(arrList);
 
