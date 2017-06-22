628320752bc468f91b55d94b861304143bda40b6
kimmynughwan <ralphmh@gmail.com>
2017-06-22 15:58:28 +0900

    20170622

--- iot1_report/src/report/kimmyunghwan/r0010/MapExam2.java
+++ iot1_report/src/report/kimmyunghwan/r0010/MapExam2.java
@@ -2,9 +2,8 @@ package report.kimmyunghwan.r0010;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
-public class MapExam2 extends HashMap {
+public class MapExam2 extends HashMap<String, Integer> {
 
 	// HasnMap을 상속 받아주세요.
 	// 키타입은 String, 밸류 타입은 Integer로 선언
@@ -12,42 +11,33 @@ public class MapExam2 extends HashMap {
 	// 총 10개의 키값을 생성해주세요
 	// toString()함수를 오버라이딩 하여서
 	// 해당 해쉬맵이 가지고 있는 전체 숫자를 더한 값을 출력해주세요.
-	
-	MapExam2(){
-		
+
+	MapExam2() {
+
 	}
-	
-	
+
 	public String toString() {
 		String result = "";
-		ArrayList<String> keyList = new ArrayList<>(keySet());
+		ArrayList<String> keyList = new ArrayList<>(keySet()); // 10개의 키가 생성되었다
+		int sum = 0;
 		for (int i = 0; i < keyList.size(); i++) {
 			String key = keyList.get(i);
-			String value = (String) keyList.next();
-			
-			result += value;
+			sum += get(key);
+
 		}
+		result += sum;
 		return result;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		MapExam2 me2 = new MapExam2();
-		me2.put("A", 1);
-		me2.put("B", 2);
-		me2.put("C", 3);
-		me2.put("D", 4);
-		me2.put("E", 5);
-		me2.put("F", 6);
-		me2.put("G", 7);
-		me2.put("H", 8);
-		me2.put("I", 9);
-		me2.put("J", 10);
-		
+
+		for (int i = 1; i <= 10; i++) {
+			me2.put("" + i, i);
+		}
+
 		System.out.println(me2);
-		
+
 	}
-	
-	
-	
-	
+
 }
