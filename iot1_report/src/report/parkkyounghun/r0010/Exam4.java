811fd6899d158f007a33e804ba9c9fc877eec2b8
kyounghunpark <redfila@naver.com>
2017-06-23 17:20:19 +0900

    t

--- iot1_report/src/report/parkkyounghun/r0010/Exam4.java
+++ iot1_report/src/report/parkkyounghun/r0010/Exam4.java
@@ -1,10 +1,12 @@
 package report.parkkyounghun.r0010;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Exam4 {
-
-//국제 네트워크 표준 모델인 OSI7 계층도를 작성해주세요.
-//네트워크에 대해 서술해 주세요.
-
+	public static void main(String[] args){
+		ArrayList<String> list = new ArrayList<String>();
+		//list.add("넘마!!");
+		String str = list.toString();
+		System.out.println(list);
+	}
 }
