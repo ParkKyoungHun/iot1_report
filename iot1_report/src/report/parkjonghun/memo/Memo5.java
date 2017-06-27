e3ba82fd64ec7a3b607da72a4d3f0056418130ac
parkjonghun <jonghun923@naver.com>
2017-06-27 18:36:10 +0900

    asd

---
+++ iot1_report/src/report/parkjonghun/memo/Memo5.java
@@ -0,0 +1,16 @@
+package report.parkjonghun.memo;
+
+public class Memo5 {
+//	외부의 자료파일 다운 받아서 설정하기.
+//	구아바 설치 방법
+//	1. 구글에 google guava download jar 로 검색.
+//	2. Maven Repository: com.google.guava » guava » 16.0.1 페이지에 입장.
+//	3. 다운로드 : http://central.maven.org/maven2/com/google/guava/guava/16.0.1/guava-16.0.1.jar
+//	4. c드라이브 -> java -> jdk1.8.9_131 -> jre -> lib -> ext 폴더에 guava-16.0.1 압축파일을 복사한다.
+//	5. 이클립스를 재시작하여 iot1_report 프로젝트 클릭->JRE System Libray[jdk1.8.0_131] 을 클릭하여 
+//	guava-16.0.1가 생성되었는지 확인한다.
+
+
+// 이것도 위와 동일한 방법으로 다운로드 mariadb-java-client-2.0.2
+
+}
