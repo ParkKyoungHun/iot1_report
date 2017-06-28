79bb02402120b9469a388ec87ff0afcc6b60c18c
kyounghunpark <redfila@naver.com>
2017-06-28 15:51:51 +0900

    t

--- iot1_report/src/common/Exam.java
+++ iot1_report/src/common/Exam.java
@@ -1,18 +1,58 @@
 package common;
 
-public class Exam {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class Exam {
+	public List<String> getUserIDLists(String name){
+		//String result = "";
+		List<String> userlist = new ArrayList<String>();
+		try {
+			Connection con = DBConn2.getCon();			
+			String sql = "select ID,PWD,NAME from user";
+			if(!name.equals("")){
+				sql += " WHERE NAME='" + name + "'";
+			}
+			PreparedStatement prestmt = con.prepareStatement(sql);
+			ResultSet rs = prestmt.executeQuery();
+			while (rs.next()) {		
+				userlist.add(rs.getString(1) + "," + rs.getString(2) + "," + rs.getString(3));
+			}			
+			DBConn2.closeCon();
+			return userlist;
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public boolean insertUser(){
+		try {
+			Connection con = DBConn2.getCon();			
+			String sql = "insert into user(id, pwd, name, age)values('blue','blue','청길동',40)";
+			PreparedStatement prestmt = con.prepareStatement(sql);
+			int result = prestmt.executeUpdate();
+			DBConn2.closeCon();
+			if(result==1){
+				return true;
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
 	public static void main(String[] args){
-		DBConn dbc = new DBConn();
-		dbc.a = 3;
-		System.out.println(dbc.a);
-		System.out.println(DBConn.a);
-		DBConn.a = 4;
-		System.out.println(dbc.a);
-		dbc = new DBConn();
-		System.out.println(dbc.a);
-		int a = DBConn.getInt();
-		
-		Integer.parseInt("asdf");
+		Exam e = new Exam();
+		e.insertUser();
+		List<String> userList = e.getUserIDLists("");
+		for(int i=0;i<userList.size();i++){
+			System.out.println(userList.get(i));
+		}
 	}
 }
