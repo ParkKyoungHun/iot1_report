be35fc37d3c103c6204ba8c900b5e0a36fec347f
jaehyunjang <4jh8@naver.com>
2017-06-28 10:46:14 +0900

    2017-06-27

---
+++ iot1_report/src/report/jangjaehyun/r0013/minus.java
@@ -0,0 +1,29 @@
+package report.jangjaehyun.r0013;
+
+
+public class minus implements InterfaceExam{
+	
+	@Override
+	public String getString() {
+		return " Exam의 getString() 함수 호출 !! " ;
+		
+	}
+	@Override
+	public void setString(String str) {
+		System.out.println(" Exam의 SetStrng() 함수 호출 !! " + str);
+	}
+	
+	public int cal(int a, int b){
+		return a-b;
+	}
+	
+
+//	public int getInt(){
+//		System.out.println( " Exam의 getInt() 함수 호출!! ");
+//		return 0;
+//	}
+//	
+//	public void setInt(int str){
+//		System.out.println(" Exam의 setInt() 함수 호출!!" + str);
+//	}
+}
