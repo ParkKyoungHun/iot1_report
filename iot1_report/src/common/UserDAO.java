277ca63aba627e698da921f46f31bd8786abf0bf
kyounghunpark <redfila@naver.com>
2017-06-29 18:33:56 +0900

    t

--- iot1_report/src/common/UserDAO.java
+++ iot1_report/src/common/UserDAO.java
@@ -7,7 +7,9 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 public class UserDAO {
 	public List<HashMap> doSelect(String sql) {
@@ -35,16 +37,44 @@ public class UserDAO {
 		return null;
 	}
 	
+
+	public boolean doInsert(String sql, HashMap<String, String> hm) {
+		try {
+			Connection con = DBConn2.getCon();
+			String[] keys = hm.keySet().toArray(new String[hm.size()]);
+			for(int i=0;i<keys.length;i++){
+				sql += keys[i] + ",";
+			}
+			sql = sql.substring(0, sql.length()-1);
+			sql += ") values(";
+
+			for(int i=0;i<keys.length;i++){
+				sql += "?,";
+			}
+			sql = sql.substring(0, sql.length()-1);
+			sql += ")";
+			PreparedStatement prestmt = con.prepareStatement(sql);
+			for(int i=0;i<keys.length;i++){
+				prestmt.setString(i+1, hm.get(keys[i]));
+			}
+			int result = prestmt.executeUpdate();
+			DBConn2.closeCon();
+			if (result == 1) {
+				return true;
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 	public static void main(String[] args){
 		UserDAO ud = new UserDAO();
-		String sql = "select * from user order by age desc";
-		List<HashMap> userList = ud.doSelect(sql);
-		System.out.println(" = 유저 리스트 = ");
-		for(HashMap hm : userList){
-			System.out.println(hm);
-		}
-		
-		sql = "insert into user_info";
+//		String sql = "select * from user order by age desc";
+//		List<HashMap> userList = ud.doSelect(sql);
+//		System.out.println(" = 유저 리스트 = ");
+//		for(HashMap hm : userList){
+//			System.out.println(hm);
+//		}
 		
 		HashMap<String, String> hm = new HashMap<String, String>();
 		hm.put("id", "green");
@@ -52,5 +82,19 @@ public class UserDAO {
 		hm.put("name", "녹길동");
 		hm.put("age", "21");
 		hm.put("class_num", "3");
+		
+
+		String sql = "insert into user_info(";
+		//ud.doInsert(sql, hm);
+		System.out.println(sql);
+		
+		sql = "insert into class_info(";
+		hm = new HashMap<String, String>();
+		hm.put("class_name", "미술반");
+		ud.doInsert(sql, hm);
+//		while(it.hasNext()){
+//			String key = (String) it.next();
+//			System.out.println(key);
+//		}
 	}
 }
