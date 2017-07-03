019e1bb1d4c7df5148b91336f57c306f5594470d
kyounghunpark <redfila@naver.com>
2017-07-03 17:38:47 +0900

    t

--- iot1_report/src/dao/BoardDAO.java
+++ iot1_report/src/dao/BoardDAO.java
@@ -15,26 +15,44 @@ public class BoardDAO {
 		con = DBConn2.getCon();
 	}
 	
-	public boolean insertBoard() throws SQLException{
+	public boolean insertBoard() {
 		String sql = "insert into board(title, content, writer, reg_Date)values('게시판제목3','게시판내용3',5,now())";
-		Statement st = con.createStatement();
-		int result = st.executeUpdate(sql);
-		if(result==1){
-			st.close();
-			st = null;
-			return true;
+		try{
+			Statement st = con.createStatement();
+			int result = st.executeUpdate(sql);
+			if(result==1){
+				con.commit();
+				st.close();
+				st = null;
+				return true;
+			}
+		}catch(Exception e){
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 		}
 		return false;
 	}
 	
-	public boolean updateBoard() throws SQLException{
+	public boolean updateBoard(){
 		String sql = "update board1 set title='으하하하하' where num='1'";
-		Statement st = con.createStatement();
-		int result = st.executeUpdate(sql);
-		if(result==1){
-			st.close();
-			st = null;
-			return true;
+		try{
+			Statement st = con.createStatement();
+			int result = st.executeUpdate(sql);
+			if(result==1){
+				con.commit();
+				st.close();
+				st = null;
+				return true;
+			}
+		}catch(Exception e){
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 		}
 		return false;
 	}
@@ -45,15 +63,8 @@ public class BoardDAO {
 			bdao.setConnection();
 			bdao.insertBoard();
 			bdao.updateBoard();
-			bdao.con.commit();
 			System.out.println("정상동작 했고 저장까지 완료 했수다!!");
 		} catch (ClassNotFoundException | SQLException e) {
-			try {
-				bdao.con.rollback();
-				System.out.println("뭔진 모르겠는데 에러났어 그래서 원복했수다!!");
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
 			e.printStackTrace();
 		}
 	}
