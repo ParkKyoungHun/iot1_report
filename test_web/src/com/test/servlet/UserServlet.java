75f78a6ac994fc348526f32dec7926077b25ebb2
DJA <DJA@DJA-PC>
2017-07-05 12:32:44 +0900

    t

--- test_web/src/com/test/servlet/UserServlet.java
+++ test_web/src/com/test/servlet/UserServlet.java
@@ -2,10 +2,14 @@ package com.test.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.test.service.UserService;
 
 public class UserServlet extends HttpServlet{
 	
@@ -20,6 +24,15 @@ public class UserServlet extends HttpServlet{
 		String class_num = req.getParameter("class_num");
 		String age = req.getParameter("age");
 		System.out.println(id + "," + pwd + "," + name + "," + class_num + ", " + age);
+		HashMap hm = new HashMap();
+		hm.put("id", id);
+		hm.put("pwd", pwd);
+		hm.put("name", name);
+		hm.put("class_num", class_num);
+		hm.put("age", age);
+		UserService us = new UserService();
+		us.insertUser(hm);
+		
 	}
 	
 	public void dePost(HttpServletRequest req, HttpServletResponse reqs) throws IOException{
