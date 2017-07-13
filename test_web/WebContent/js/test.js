4f6b9006b2515590fe57da37a3105495fe3bc804
DJA <DJA@DJA-PC>
2017-07-13 12:47:44 +0900

    t

--- test_web/WebContent/js/test.js
+++ test_web/WebContent/js/test.js
@@ -2,30 +2,34 @@
  * 
  */
 
-function checkValue(fObj){
+function checkValue(fObj) {
 	var maxNum = fObj.elements.length;
 
 	var result = "";
-	for(var i=0;i<maxNum;i++){
+	for (var i = 0; i < maxNum; i++) {
 		var eObj = fObj.elements[i];
-		if(eObj.value.indexOf("빡쌤")!=-1){
-			alert("뒤질라고!!");
-			eObj.value = "";
-			eObj.focus();
-		}
-		if(i%2==1 && i==maxNum-1){
+//		if (eObj.value.indexOf("빡쌤") != -1) {
+//			alert("뒤질라고!!");
+//			eObj.value = "";
+//			eObj.focus();
+//		}
+		if (i % 2 == 1 && i != maxNum - 1) {
 			var checkNum = new Number(eObj.value);
-			if(isNaN(checkNum)){
-				alert("숫자라고!!!!!!!!!");
-				eObj.value = "";
-				eObj.focus();
-				return false;
+			try{
+				if (isNan(checkNum)) {
+					alert("숫자라고!!!!!!!!!");
+					eObj.value = "";
+					eObj.focus();
+					return false;
+				}
+			}catch(e){
+				alert(e);
 			}
 		}
-		if(eObj.value!="전송"){
+		if (eObj.value != "전송") {
 			result += eObj.value;
 		}
-		if(i==maxNum-1){
+		if (i == maxNum - 1) {
 			eObj.value = result;
 		}
 	}
@@ -33,6 +37,6 @@ function checkValue(fObj){
 	var eObj2 = fObj.elements[1];
 	var eObj3 = fObj.elements[3];
 	eObj3.value = eObj1.value + eObj2.value;
-	
+
 	return false;
 }
