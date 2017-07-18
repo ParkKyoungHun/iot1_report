bd75b9b2d0c0a61b73249ae0cc01de2fe444eecb
DJA <DJA@DJA-PC>
2017-07-18 17:18:01 +0900

    1

--- test_web/WebContent/js/Ajax.js
+++ test_web/WebContent/js/Ajax.js
@@ -13,17 +13,32 @@ function getHttpXmlObj(){
 	alert("해당 브라우져가  Ajax를 지원하지 않습니다.");
 }
 
-var AjaxUtil = function(){
-	var str = "난 괜찮겠지?";
-	this.str2 = "나도 문잔데?";
-	this.alertVar = function(){
-		alert(str);
-		alert(this.str2);
-	}
-	this.getStr = function(){
-		return str;
-	}
+var str = "name,id,pwd";
+var strs = str.split(",");
+var param = "";
+for(var i=0;i<strs.length;i++){
+	var value = document.getElementById(strs[i]).value;
+	param += "&" + strs[i] + "=" + value;
 }
-
-var au = new AjaxUtil();
-alert(au.getStr());
+var au = new AjaxUtil("/login.action","name,id,pwd");
+var AjaxUtil = function(url, arrParams, method,  aSync){
+	this.fAction = url;
+   	this.fMethod = method ? method : "get";
+   	var params = "?action=LOGIN&id=" + encodeURIComponent(userid);
+   	this.fASync = aSync ? aSync : true;
+   	xmlHttpObj.onreadystatechange=function(){
+   		if (xmlHttpObj.readyState==4 && xmlHttpObj.status==200){
+   			var result = decodeURIComponent(xmlHttpObj.responseText);
+   			if(result == "success"){
+       			location.href = "../user/welcome.jsp"
+       		}else{
+       			alert(result);
+       		}
+        }
+    }
+   	xmlHttpObj.open(method, url+params, sync);
+   	if(method=="post"){
+   		xmlHttpObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+   	}
+   	xmlHttpObj.send(params);
+}
