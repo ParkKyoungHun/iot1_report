d22c4c5ba3d40e083492ad814456ea66527440b1
DJA <DJA@DJA-PC>
2017-07-21 17:36:20 +0900

    1

--- iot1_report/src/sql/board.sql
+++ iot1_report/src/sql/board.sql
@@ -7,7 +7,7 @@ creusr varchar(100) not null,
 credat datetime not null
 )
 /*
-board_info테이블의 crud를 만들어주시고
+board_info테이블의 insert, select, delete 만들어주시고
 삭제나 수정시에는 비밀번호를 입력하여 비밀번호가 맞을때만 삭제나 수정이 가능하게
 만들어 주세요.
 */
